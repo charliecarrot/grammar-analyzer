2e2a2369efac2a5afafad65bf083a1c3af9078ea
Charlie Davis <34718612+charliecarrot@users.noreply.github.com>
2024-02-24 19:49:10 -0700

    Add call to openAI chat completion

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
 // src/index.ts
 import express, { type Express, type Request, type Response } from 'express'
 import dotenv from 'dotenv'
+import OpenAI from 'openai'
+import { complete } from './openai/openai'
 
 dotenv.config()
 
 const app: Express = express()
 const port = process.env.PORT || 3000
 
-// Middleware to parse JSON body
+// Middleware
 app.use(express.json())
-
-// Routes
 
 // POST
 app.post('/', async (req: Request, res: Response) => {
@@ -21,10 +21,15 @@ app.post('/', async (req: Request, res: Response) => {
     }
 
     try {
-        return res.status(200).json({
-            success: true,
-            message: sentence,
+        const openai = new OpenAI({
+            apiKey: process.env['OPENAI_API_KEY'],
         })
+        const response = await complete(openai, sentence)
+        if (!response.success) {
+            throw new Error(response.message)
+        } else {
+            return res.status(200).json(response)
+        }
     } catch (error) {
         console.error(error)
         return res.status(500).json({ success: false, message: 'Failed to process sentence.' })
